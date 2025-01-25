408f07b11c06f6c9023655c8d0d4ced0e6654040
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-25 12:31:04 +0530

    Update EvenNumberSum.java

--- EvenNumberSum.java
+++ EvenNumberSum.java
@@ -1,15 +1,12 @@
 public class EvenNumberSum {
     public static void main(String[] args) {
         int sum = 0;
-        int number = 2; // Start with the first even number
-
-        // Use a while loop to sum even numbers between 1 and 100
+        int number = 2; 
         while (number <= 100) {
-            sum += number; // Add the even number to sum
-            number += 2;   // Move to the next even number
+            sum += number; 
+            number += 2;   
         }
 
-        // Display the result
         System.out.println("The sum of all even numbers between 1 and 100 is: " + sum);
     }
 }
